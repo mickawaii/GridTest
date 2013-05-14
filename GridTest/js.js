45a07b507449472070c5136a3e8c62026cecb260
Mi Che Li Lee <mi.lee@codus.com.br>
2013-05-14 20:56:49 -0300

    acabei de terminar o menu dinamico

---
+++ GridTest/js.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+// 	$(".menu_flutuante").hover(
+// 	  function () {
+// 	    $(this).animate({ width: '150px'}, function(){
+// 	    	$(".sub_menu").slideDown();	
+// 	    });
+// 	  },
+// 	  function () {
+// 	  	$(".sub_menu").slideUp(400, function(){
+// 	  		$(".menu_flutuante").animate({ width: '100px'});
+// 	  	});
+// 	  }
+// 	);
+// });
